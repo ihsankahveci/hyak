5e964e0b490fb33a0ee0a3c2ae72150fbb1ea266
ihsankahveci <ikahveci@uw.edu>
2021-06-29 14:52:25 +0300

    Julia's solution

---
+++ manyModels.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(stm)
+library(furrr)
+plan(multicore)
+
+out <- readRDS(file = "out.rds")
+docs <- out$documents
+vocab <- out$vocab
+meta  <- out$meta
+
+#finding the optimal number of K
+many_models <- 
+  tibble(K = 3:50) %>%
+  mutate(topic_model = future_map(K, ~stm(docs, vocab, data = meta,
+                                          K = ., 
+                                          prevalence = ~ramadan+sacrifice+budget_share+republic+kurdish+civilians+turkish+location+unemployment,  
+                                          seed = 57, 
+                                          verbose=T)))
+
+#saving the outputs
+saveRDS(many_models, "manyModels.rds")
+
