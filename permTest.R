4b5d96e2e2d8755d1a57e88acf4ce4b01b1d9ef3
ihsankahveci <ikahveci@uw.edu>
2021-07-09 23:11:02 +0300

    fixed the plot functuon

--- permTest.R
+++ permTest.R
@@ -1,10 +1,10 @@
-library(tidyverse)
+library(dplyr)
 library(stm)
 
 out <- readRDS(file = "out.rds")
 docs <- out$documents
 vocab <- out$vocab
-meta  <- out$meta %>% mutate(turkish = ifelse(as.numeric(turkish) > 2, 1, 0))
+meta  <- mutate(out$meta, turkish = ifelse(as.numeric(turkish) > 2, 1, 0))
 
 stm_perm = stm(docs, vocab, data = meta,
                K = 22,  
@@ -20,6 +20,6 @@ perm_test = permutationTest(~holy_days+budget_share+turkish+location+unemploymen
 
 #saving the outputs
 pdf("perm_plot.pdf")
-plot(perm_test)
+plot(perm_test, topic = 10)
 dev.off()
 saveRDS(perm_test, "perm_test.rds")
