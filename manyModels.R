efd9b8d620240fb6bc73aacb7e5b7a4c02aacecf
ihsankahveci <ikahveci@uw.edu>
2021-08-05 01:39:58 +0300

    add progressor to the many models

--- manyModels.R
+++ manyModels.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(stm)
 library(furrr)
+library(progressr)
 plan(multicore)
 
 out <- readRDS(file = "out.rds")
@@ -8,15 +9,26 @@ docs <- out$documents
 vocab <- out$vocab
 meta  <- out$meta
 
+raw_df = tibble(K = 3:50)
+
+stm_fn <- function(k, p, docs, vocab, meta){
+  out <- stm(docs, vocab, data = meta,
+             K = k, 
+             prevalence = ~holy_days+budget_share+republic+kurdish+civilians+turkish+location+unemployment,  
+             seed = 57, verbose=FALSE)
+  p()
+  return(out)
+}
+
+with_progress({
+  p <- progressor(steps = nrow(raw_df))
 #finding the optimal number of K
-many_models <- 
-  tibble(K = 3:50) %>%
-  mutate(topic_model = future_map(K, ~stm(docs, vocab, data = meta,
-                                          K = ., 
-                                          prevalence = ~holy_days+budget_share+republic+kurdish+civilians+turkish+location+unemployment,  
-                                          seed = 57, 
-                                          verbose=T
-                                          ), .options = furrr_options(seed = TRUE)))
+  many_models <- 
+    raw_df %>%
+    mutate(topic_model = future_map(K, ~stm_fn(k=.,p=p, docs, vocab, meta),
+                                    .options = furrr_options(seed = TRUE)))
+})
+
 #saving the outputs
 saveRDS(many_models, "manyModels.rds")
 
